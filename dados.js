cb4434c5c3496fec74b592bc5b7f5f60d3a6e66a
dunkschwam <luan_teamxd@hotmail.com>
2024-09-06 21:54:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Rafael Brandão",
+        descricao: "Rafael Brandão é um dos maiores nomes do fisiculturismo brasileiro. O atleta, conhecido por sua dedicação e disciplina, conquistou diversos títulos importantes ao longo de sua carreira, incluindo a marca de ser o primeiro brasileiro a vencer um campeonato profissional da Open Bodybuilding.",
+        link: "https://pt.wikipedia.org/wiki/Rafael_Brandão"
+    },
+    {
+        titulo: "Gabriel Zancanelli",
+        descricao: "Gabriel Zancanelli é um dos nomes mais promissores do fisiculturismo brasileiro. Conhecido por sua estética clássica e simetria muscular, ele vem conquistando cada vez mais fãs e respeito no cenário nacional.",
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Zancanelli"
+    },
+    {
+        titulo: "Felipe Franco",
+        descricao: "Felipe Franco é uma verdadeira lenda viva do fisiculturismo brasileiro. Com anos de experiência e diversos títulos em seu currículo, ele é uma inspiração para muitos atletas e um dos maiores embaixadores do esporte no país.",
+        link: "https://pt.wikipedia.org/wiki/Felipe_Franco"
+    }
+];
+
